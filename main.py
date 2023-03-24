11bfabdabc30aad20862eea43f128f882afe2373
LuisanaHenmary <luisanahenmaryp@gmail.com>
2023-03-24 10:59:24 -0400

    Update main file

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-from generate_bingo import Game
-import os
+from game import Game
 import time
-import json
+import random
 
 if __name__=="__main__":
 
-    game_mode = int(input("""Ingrese la modalidad de juego
+    game = Game()
+
+    while True:
+        game_mode = int(input("""Ingrese la modalidad de juego
 1) Lograr 5 en linea.
 2) Llenar todo su carton.
     """))
 
-    print(game_mode)
+        for i in range(2):
+            game.input_data()
 
-    game = Game()
-
-    
-    os.system("cls")
-    for i in range(2):
-        game.input_data()
-    
-    game.print_boards()
-
-    for i in range(75):
-        game.move(game_mode)
-        time.sleep(2)
         game.print_boards()
-        if game.say_bingo():
-            break
-
-
-        
     
+        for i in range(75):
+            print(game.num_available_combinations())
+            index = random.randint(0,game.num_available_combinations()-1)
+            game.move(game_mode,index)
+            time.sleep(2)
+            game.print_boards()
+            if game.say_bingo():
+                break
 
-    game.diagonal_validation()
-        
+        responce = int(input("""¿Quiere Parar?
+1) Si.
+2) No.
+    """))
+
+        game.reboot()
+        if responce == 1:
+            break
